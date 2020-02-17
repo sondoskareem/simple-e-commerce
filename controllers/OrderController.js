50581cb67df0ee7d82c374d37972cd8212a03d28
sondoskareem <sosoka263@gmail.com>
2020-02-18 00:38:18 +0300

    fina

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -19,7 +19,7 @@ exports.add_order =  (req, res) =>{
       })
     }else{
       // if(req.files.file){
-      console.log(req.files.file)
+      // console.log(req.files.file)
       console.log(req.body)
 
       //   var file = req.files.file;
