126d18d0b45000e51556df6ee6bb14c346f2ea0d
sondoskareem <sosoka263@gmail.com>
2020-03-19 23:48:22 +0300

    fina

--- functions/send_email.js
+++ functions/send_email.js
@@ -10,8 +10,8 @@ exports.send_email  = function (email , id , req , res , successMsg){
 	var transporter = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
-		  user: 'shoppingorder8',
-		  pass: 'AAgHk2^#;'
+		  user: 'shoppingorder8@gmail.com',
+		  pass: 'Baghdad111'
 		},
 		tls: {
 		  rejectUnauthorized: false
