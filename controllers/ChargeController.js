da532147f9f5fa22cc6d16a805cdb6014f7561d6
sondoskareem <sosoka263@gmail.com>
2020-03-29 18:12:14 +0300

    final

--- controllers/ChargeController.js
+++ controllers/ChargeController.js
@@ -2,8 +2,8 @@
 
 // var secret_key = process.env.SK_TEST
 // var public_key = process.env.PK_TEST
-var secret_key ='sk_test_RLPuAioyNkHCDVxT2gunsnga007Susm4dC'
-var public_key ='pk_test_H9u1xitwtI7KnNrc2YFCdz2w00WdcqWP8S'
+var secret_key ='xxxxxxxx'
+var public_key ='xxxxxxx'
 const stripe = require('stripe')(secret_key)
 // Charge Route
 
